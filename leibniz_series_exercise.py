5f80f31799cdad0a9c89c9fa9ce3baf491594205
carolmetanis3105 <carol.metanis@gmail.com>
2025-11-13 08:27:46 +0200

    Update leibniz_series_exercise.py

--- leibniz_series_exercise.py
+++ leibniz_series_exercise.py
@@ -1,13 +1,9 @@
-import random
+def estimate_pi(num_terms):
+   leibniz_series = []
+   for i in range(n_terms):
+       leibniz_series.append((-1)**i / (2*i + 1))
+    sum_numbers = sum( leibniz_series)
 
-def estimate_pi(num_of_points):
-    count_in_circle = 0
-    for _ in range(num_of_points):
-        radius = 0.5
-        x = random.random()
-        y = random.random()
-        distance_to_center = ((x - radius)**2 + (y - radius)**2)**0.5
-        if distance_to_center <= radius:
-            count_in_circle += 1
-    pi = 4 * count_in_circle / num_of_points
-    return pi
+    approximate_pi = 4 * (sum_numbers)
+    return approximate_pi
+
